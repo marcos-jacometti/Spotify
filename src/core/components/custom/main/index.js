a91ed2e7ef83b81ae4bebde79a8135d540582937
Marcos Vinícius Jacometti <marcosjacometti10@gmail.com>
2022-01-30 21:52:44 -0300

    main

--- src/core/components/custom/main/index.js
+++ src/core/components/custom/main/index.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Content, Wrapper } from "./styles";
+import { VscChevronRight, VscChevronLeft } from "react-icons/vsc";
+import { AiOutlineUser } from "react-icons/ai";
 
 export default function Main(){
     return(
-        <Container />
+        <Container>
+            <Wrapper>
+                <Content>
+                    <Content width="8%" height="6vh" background="" justify="space-around">
+                        <Content height="3.2vh" width="1.6vw" background="#000" radius="50%" justify="center"><VscChevronLeft /></Content>
+                        <Content height="3.2vh" width="1.6vw" background="#000" radius="50%" justify="center"><VscChevronRight /></Content>
+                    </Content>
+                    <Content width="92%" height="6vh" justify="end">
+                        <Content background="#000" radius="5vh" width="13%" height="3vh">
+                            <AiOutlineUser />
+                            <h4>Marcos Jacometti</h4>
+                        </Content>
+                    </Content>
+                </Content>
+            </Wrapper>
+        </Container>
     );
 }
--- src/core/components/custom/main/styles.js
+++ src/core/components/custom/main/styles.js
@@ -1,8 +1,34 @@
 import styled from "styled-components";
+import img from "../../../img/background.jpg";
 
 export const Container = styled.main`
     height: 100vh;
     width:70vw;
     background: #181818;
     float: left;
+`;
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 44vh;
+    background-image: url(${img});
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+`;
+
+export const Content = styled.div`
+    width: ${props => props.width};
+    height: ${props => props.height};
+    display: flex;
+    align-items: center;
+    flex-direction: ${props => props.direction};
+    justify-content: ${props => props.justify};
+    background: ${props => props.background};
+    border-radius: ${props => props.radius};
+    color: #fff;
+
+    svg {
+        fill: #fff;
+        font-size: 2.5vh;
+    }
 `;
